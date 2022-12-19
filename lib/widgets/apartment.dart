80f626a84ed1c72c74608abb62fb2d3046dca8e2
Maximilian Kern <maxkernw@gmail.com>
2022-12-19 23:26:18 +0100

    Add second route

--- lib/widgets/apartment.dart
+++ lib/widgets/apartment.dart
@@ -15,31 +15,36 @@ class _ApartmentCardState extends State<ApartmentCard> {
   @override
   Widget build(BuildContext context) => ClipRRect(
       borderRadius: BorderRadius.circular(20),
-      child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(widget.user.image),
-                  fit: BoxFit.cover,
-                  alignment: const Alignment(-0.3, 0))),
+      child: GestureDetector(
           child: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.transparent, Colors.black],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.7, 1])),
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  const Spacer(flex: 2),
-                  buildName(),
-                  const SizedBox(height: 8),
-                  buildAdress()
-                ],
-              ),
-            ),
-          )));
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(widget.user.image),
+                      fit: BoxFit.cover,
+                      alignment: const Alignment(-0.3, 0))),
+              child: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.transparent, Colors.black],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.7, 1])),
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      const Spacer(flex: 2),
+                      buildName(),
+                      const SizedBox(height: 8),
+                      buildAdress()
+                    ],
+                  ),
+                ),
+              )),
+          onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()),
+              )));
 
   Widget buildName() => Row(children: [
         Text(widget.user.name,
@@ -56,4 +61,23 @@ class _ApartmentCardState extends State<ApartmentCard> {
           style: const TextStyle(fontSize: 32, color: Colors.white),
         )
       ]);
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
 }
